4a5656d8663a4e41e3a73ef2ea0c5f73d33c24c9
haohailiang <646263409@qq.com>
2017-03-28 10:07:46 +0800

    修改webpack.config.js文件
    1.多个JS文件合并成一个文件
    2.忽略dist文件夹提交
    3.忽略node_modules文件夹

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,7 @@
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-
 module.exports = {
-  // entry: ["./src/script/main.js","./src/script/a.js"],
-  entry:{
-    mainKey:"./src/script/main.js",
-    aKey:"./src/script/a.js"
-  },
+  entry: ["./src/script/main.js","./src/script/a.js"],
   output: {
-    path: __dirname + "/dist/js",
-    filename: '[name]-[chunkhash][5].bundle.js' //这样名字就不会相互覆盖
-  },
-  plugins: [new HtmlWebpackPlugin()]
+    path: "./dist/js",
+    filename: 'main.bundle.js'
+  }
 }
