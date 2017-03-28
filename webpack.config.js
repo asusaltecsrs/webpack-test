394c50dbbb97416d9f7a18c6c4d49122bca91177
haohailiang <646263409@qq.com>
2017-03-28 10:30:16 +0800

    把代码插入到index.html文件当中
    HtmlWebpackPlugin来实现，但是生成的index.html文件与源文件没有关联，都生成到dist文件当中了

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   entry: {
     mainKey:"./src/script/main.js",
@@ -6,5 +8,6 @@ module.exports = {
   output: {
     path: "./dist/js",
     filename: '[name]-[chunkhash].bundle.js'
-  }
+  },
+  plugins: [new HtmlWebpackPlugin()]
 }
